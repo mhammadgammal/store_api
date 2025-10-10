c4854f710f3b9bf0de2080a382377c371b1aa309
mhammadgammal <mhammadgamal17@gmail.com>
2025-10-10 14:09:15 +0300

    change approach of returning right response error form if-else to switch case

--- middleware/errorHandlerMiddleware.js
+++ middleware/errorHandlerMiddleware.js
@@ -1,23 +1,31 @@
 import NotFoundException from '../exception/notFoundExeption.js';
+import DuplicateEntryException from '../exception/duplicateEntryException.js';
 
 const errorHandlerMiddleware = (err, req, res, next) => {
     console.error(`Error:\n ${err.message}\n Stack: ${err.stack}`);
 
-    if (err instanceof NotFoundException) {
-        res.status(404).json(
-            {
+    switch (true) {
+        case err instanceof NotFoundException:
+            res.status(404).json({
                 status: 'error',
                 message: err.message
-            }
-        );
-    } else {
-        res.status(500).json(
-            {
+            });
+            break;
+
+        case err instanceof DuplicateEntryException:
+            res.status(409).json({
+                status: 'error',
+                message: err.message
+            });
+            break;
+
+        default:
+            res.status(500).json({
                 status: 'error',
                 message: 'Internal Server Error',
                 error: err.message || 'An unexpected error occurred'
-            }
-        )
+            });
+            break;
     }
 }
 
