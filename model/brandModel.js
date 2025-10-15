33e282d9084984d6813318ff39ff0a632a2e76ab
mhammadgammal <mhammadgamal17@gmail.com>
2025-10-16 02:02:28 +0300

    implement a brand model

---
+++ model/brandModel.js
@@ -0,0 +1,18 @@
+import mongoose, { Mongoose, Schema, model } from 'mongoose';
+
+const brandSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Brand name is required'],
+        trim: true,
+        minLength: [3, 'Brand name must be at least 3 characters long'],
+        maxLength: [50, 'Brand name must not exceed 50 characters'],
+        unique: true,
+    },
+    image: String,
+});
+
+const Brand = model('Brand', brandSchema);
+
+// Export the model as default and expose commonly used model static helpers as named exports
+export default Brand;
